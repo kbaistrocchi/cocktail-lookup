4a806a2c7ed772b5e32358f804459ce00d794946
Kayla <kbaistrocchi@gmail.com>
2021-03-23 14:40:44 -0400

    chore: add node-sass to project and update file types

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import SearchForm from './components/search-form/search-form.component.jsx';
 
 function App() {
   return (
