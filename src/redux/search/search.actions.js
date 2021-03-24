0f7f254c0fd8d2d6077c9b158a7fbf1e464de20f
Kayla <kbaistrocchi@gmail.com>
2021-03-23 20:16:18 -0400

    feat: create actions for async api call to match cases in reducer

--- src/redux/search/search.actions.js
+++ src/redux/search/search.actions.js
@@ -2,3 +2,33 @@ export const setSearchTerm = term => ({
     type: 'SET_SEARCH_TERM',
     payload: term
 });
+
+export const fetchItemsStart = () => ({
+    type: 'FETCH_ITEMS_START'
+})
+
+export const fetchItemsSuccess = itemsMap => ({
+    type: 'FETCH_ITEMS_SUCCESS',
+    payload: itemsMap
+})
+
+export const fetchItemsFailure = errorMessage => ({
+    type: 'FETCH_ITEMS_FAILURE',
+    payload: errorMessage
+})
+
+export const fetchItemsStartAsync = (term) => {
+    console.log("term", term);
+    const apiRoot = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i="
+    const filteredApi = apiRoot.concat(term);
+    console.log("api", filteredApi);
+    return dispatch => {
+        dispatch(fetchItemsStart());
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${term}`)
+            .then(res => res.json())
+            .then((result) => {
+                const itemsMap = result.drinks;
+                dispatch(fetchItemsSuccess(itemsMap));
+            }).catch(error => dispatch(fetchItemsFailure(error.message)));
+    }
+}
