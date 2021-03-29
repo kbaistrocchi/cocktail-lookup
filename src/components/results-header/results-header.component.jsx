36f6306cacc45a65deecef0e36d759cfe5c0753d
Kayla <kbaistrocchi@gmail.com>
2021-03-29 19:16:29 -0400

    feat: conditionally display search header depending on results and search terms

--- src/components/results-header/results-header.component.jsx
+++ src/components/results-header/results-header.component.jsx
@@ -2,40 +2,56 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SearchResultsHeader = ({
-    errorMessage
+    errorMessage,
+    filteredItems,
     latestSearchTerm,
     latestSecondaryTerm,
     primarySearchItems,
-    secondarySearchItems,
-    secondarySearchTerm
 }) => {
-    let count1 = primarySearchItems.length;
+    let count = primarySearchItems.length;
     // There are drinks with both items
-    if (latestSecondaryTerm.length > 0) {
-        return <p></p>
+    if (filteredItems.length > 0) {
+        return (
+            <h2>
+                We found {filteredItems.length} {" "}
+                {filteredItems.length > 1 ? "drinks" : "drink"} containing both {" "}
+                {latestSearchTerm} and {latestSecondaryTerm}!
+            </h2>
+        )
     }
     // No drinks with both items
-    // Drinks with only first item
-    
-    if (count1 > 0) {
-        return <p>We found {count} cocktails containing {latestSearchTerm}</p>;
+    else if (latestSecondaryTerm.length > 0) {
+        return (
+            <h2>
+                Sorry, we can't find any drinks that contain both {" "}
+                {latestSearchTerm} and {latestSecondaryTerm}. Try
+                a different ingredient to refine your search. Maybe "lime"
+                 or "salt"?
+            </h2>
+        )
+    }
+    // Only 1 item searched and contains drinks
+    else if (count > 0) {
+        return <h2>We found {count} {" "} {count > 1 ? "cocktails" : "cocktail"} containing {latestSearchTerm}</h2>;
     }
     // No drinks with first item
-    else if (latestSearchTerm.length < 1) {
-        return <p>Try searching for something, for example "gin"</p>
+    else if (latestSearchTerm.length > 0) {
+        return <h2>Sorry, we couldn't find any cocktails containing "{latestSearchTerm}"</h2>
     }
     // No search term entered
-    else return <p>Sorry, we couldn't find any cocktails containing "{latestSearchTerm}"</p>;
+    else if (latestSearchTerm.length < 1) {
+        return <h2>Try searching for something, for example "gin"</h2>
+    }
+    else return <h2>Error: {errorMessage}</h2>;
 
 }
 
 const mapStateToProps = state => ({
     errorMessage: state.search.errorMessage,
+    filteredItems: state.search.filteredItems,
     latestSearchTerm: state.search.latestSearchTerm,
     latestSecondaryTerm: state.search.latestSecondaryTerm,
     primarySearchItems: state.search.primarySearchItems,
-    secondarySearchItems: state.search.secondarySearchItems,
-    secondarySearchTerm: state.search.secondarySearchTerm
 })
 
 export default connect(mapStateToProps)(SearchResultsHeader);
