e4eae40aaca23f76b96070262c98fa4a5ab53b00
Kayla <kbaistrocchi@gmail.com>
2021-03-29 18:55:34 -0400

    feat: move search results header and write out logic for next steps

--- src/components/results-header/results-header.component.jsx
+++ src/components/results-header/results-header.component.jsx
@@ -2,19 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 const SearchResultsHeader = ({
+    errorMessage
     latestSearchTerm,
+    latestSecondaryTerm,
     primarySearchItems,
-    primarySearchTerm,
     secondarySearchItems,
     secondarySearchTerm
 }) => {
-    const count = primarySearchItems.length;
-    if (count > 0) {
+    let count1 = primarySearchItems.length;
+    // There are drinks with both items
+    if (latestSecondaryTerm.length > 0) {
+        return <p></p>
+    }
+    // No drinks with both items
+    // Drinks with only first item
+    
+    if (count1 > 0) {
         return <p>We found {count} cocktails containing {latestSearchTerm}</p>;
     }
+    // No drinks with first item
     else if (latestSearchTerm.length < 1) {
         return <p>Try searching for something, for example "gin"</p>
     }
+    // No search term entered
     else return <p>Sorry, we couldn't find any cocktails containing "{latestSearchTerm}"</p>;
 
 }
@@ -22,8 +32,8 @@ const SearchResultsHeader = ({
 const mapStateToProps = state => ({
     errorMessage: state.search.errorMessage,
     latestSearchTerm: state.search.latestSearchTerm,
+    latestSecondaryTerm: state.search.latestSecondaryTerm,
     primarySearchItems: state.search.primarySearchItems,
-    primarySearchTerm: state.search.primarySearchTerm,
     secondarySearchItems: state.search.secondarySearchItems,
     secondarySearchTerm: state.search.secondarySearchTerm
 })
