7dd961b4d7e25a0a1cfd485271e9c331c5c441a2
Kayla <kbaistrocchi@gmail.com>
2021-03-31 17:52:32 -0400

    style: app header - gold text & drink svg

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import './App.scss';
 import SearchForm from './components/search-form/search-form.component.jsx';
 import SearchResults from './components/search-results/search-results.jsx';
 
+import martini from './assets/martini.svg';
+
 function App() {
   return (
     <div className="App">
       <header>
-        <h1>Cocktail Lookup</h1>
+        <h1 className="gold">Cocktail Lookup</h1>
+        <img className="header-img" src={martini} alt="martini glass with olive"/>
       </header>
       <main>
         <SearchForm/>
