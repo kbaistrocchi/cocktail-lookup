9f7c4d954b0a1c3db40a5c3209b85dfc164d626a
Kayla <kbaistrocchi@gmail.com>
2021-04-09 12:55:11 -0400

    feat: remove middlewares from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger, thunk];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push(logger, thunk);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
